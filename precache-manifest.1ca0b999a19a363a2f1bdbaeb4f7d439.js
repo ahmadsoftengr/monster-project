b95618d4a7edf55e62c3b64c33581681908063d8
Ahmad Raza Soft.Engr <ahmad.raza916@gmail.com>
2020-03-16 23:47:09 +0500

    Updates

---
+++ precache-manifest.1ca0b999a19a363a2f1bdbaeb4f7d439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e080d6b88eaca55c4f2cfdc1b1f6790e",
+    "url": "/monster-project/index.html"
+  },
+  {
+    "revision": "1cd2b49ac99bff148f3f",
+    "url": "/monster-project/static/css/main.e4640d3e.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monster-project/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-project/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd2b49ac99bff148f3f",
+    "url": "/monster-project/static/js/main.bf7c08df.chunk.js"
+  },
+  {
+    "revision": "70188f577aec0c678db5",
+    "url": "/monster-project/static/js/runtime-main.a033152d.js"
+  }
+]);
